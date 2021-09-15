40fe6b30f55280d0d2c74754de7312cb3ae6eebd
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-15 09:12:04 -0500

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,7 +1,7 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyBFEGUWXOTv5Sgcae53-MUJmqlPce0YZ0M",
-  authDomain: ""amparo-671eb.firebaseapp.com",
+  authDomain: "amparo-671eb.firebaseapp.com",
   databaseURL: "https://amparo-671eb.firebaseio.com",
   projectId: "amparo-671eb",
   storageBucket: "amparo-671eb.appspot.com",
