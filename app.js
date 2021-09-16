ce82249529d8714f78020170a0dd0af6aca85b7e
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-16 10:41:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,34 @@ function register(){
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
+//create a functions to push
+    function firebasePush(input) {
 
+
+        //prevents from braking
+        if (!firebase.apps.length) {
+            firebase.initializeApp(config);
+        }
+
+        //push itself
+        var mailsRef = firebase.database().ref('nombre').push().set(
+            {
+                mail: input.value
+            }
+        );
+
+    }
+
+//push on form submit
+    if (form) {
+        form.addEventListener('submit', function (evt) {
+            evt.preventDefault();
+            firebasePush(inputNombre);
+
+            //shows alert if everything went well.
+            return alert('Data Successfully Sent to Realtime Database');
+        })
+    }
     
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
