a97ee52aec2a2cd199d86713b64f6b72c9295424
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-16 10:24:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,41 +17,6 @@ function register(){
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
 
-//listen for submit event//(1)
-document
-  .getElementById('registrationform')
-  .addEventListener('submit', formSubmit);
-
-//Submit form(1.2)
-function formSubmit(e) {
-  e.preventDefault();
-  // Get Values from the DOM
-  let name = document.querySelector('#name').value;
-
-  //send message values
-  sendMessage(name);
-
-  //Show Alert Message(5)
-  document.querySelector('.alert').style.display = 'block';
-
-  //Hide Alert Message After Seven Seconds(6)
-  setTimeout(function() {
-    document.querySelector('.alert').style.display = 'none';
-  }, 7000);
-
-  //Form Reset After Submission(7)
-  document.getElementById('registrationform').reset();
-}
-
-//Send Message to Firebase(4)
-
-function sendMessage(name, email, password, bio, job, interest) {
-  let newFormMessage = formMessage.push();
-  newFormMessage.set({
-    name: name
-  });
-}
-    
     
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
