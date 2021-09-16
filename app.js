bfd825b1781ee2bb9819e022a28dba760a093230
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-16 09:12:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -119,3 +119,33 @@ function show(user) {
     `;
   }
 }
+
+ &lt;script&gt;
+    const form = document.getElementById('contactForm'); // Obtenemos la referencia al formulario
+
+    if(form){ // Si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
+      form.addEventListener('submit', contactForm); // Al momento de enviar el formulario, ejecuta la función "contactform"
+    }
+
+    function contactForm(event) {
+      event.preventDefault(); // Prevenimos el comportamiento por defecto de un formulario (Enviar por URL los parametros)
+      const nombre = document.getElementById('nombre'); // Obtenemos la referencia a cada uno de nuestros elementos del formulario
+      const email = document.getElementById('apaterno');
+      const data = {
+        'name': nombre.value,
+        'apaterno': email.value,
+      }; // Creamos un objecto con todos los elementos de nuestro formulario.
+      saveContactForm(data); // Enviamos la información obtenida por el usuario a la función que se encargara de guardar la información en Firebase
+      form.reset(); // borramos todos los campos. 
+    }
+
+  function saveContactForm(data) {
+    firebase.database().ref('contact').push(data) // Hacemos referencia al método database de el SDK y hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
+      .then(function(){
+        alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+      })
+      .catch(function(){
+        alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
+      });
+  };
+  &lt;script&gt;
