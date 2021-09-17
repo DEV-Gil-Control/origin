80c296ff4f01a3dadf653ae84b3e7e6f91973686
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-17 14:52:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ function show(user) {
           <h5 class="card-header"><div>Bienvenido ${user.email}</h5> <button class="btn btn-outline-dark" onclick="singOut()">Cerrar</button></div>
           <div class="card-body">
             <p class="card-text">Ingresa a tu cuenta de correo registrada y verifica tu cuenta por favor.</p> 
-            <p><a href="https://chaledelafuente.com/docs/AMP-MENORES-DIPUTADOS-PAN.pdf">Recomendamos registrar tu correo, puedes descargar aqui el formato de amparo</p>    
+           <div><p><a href="https://chaledelafuente.com/docs/AMP-MENORES-DIPUTADOS-PAN.pdf">Recomendamos registrar tu correo, puedes descargar aqui el formato de amparo</p> </div>   
            
           <div class="container mt-5"> 
            <div class="modal-header">
