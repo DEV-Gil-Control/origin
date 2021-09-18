06b50efea3e6c1b3a02a8d93121856723fc1c4f5
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-18 09:43:01 -0500

    Update user.js

--- user.js
+++ user.js
@@ -123,13 +123,8 @@ function observer(){
       content.innerHTML = `
       <div class="container mt-5">
         <div class="card">
-          <h5 class="card-header">Bienvenido al sistema de registro y descarga de documentación, por favor sigue las siguientes instrucciones:</h5>
-          <div class="card-body">
-            <p>1. Haz clic en el botón "Registrar" en la parte superior para proceder al registro del/la menor de edad a tu cargo.</p>
-            <p>2. Una vez registrado, tendrás acceso a descargar la documentación legal, solo escribe tu correo, contraseña registrada y haz clic en "Ingresar", una vez descargado el documento en formato PDF, debes imprimirlo y firmarlo.</p>
-            <p>3. Revisa y completa la documentación que se describe al final del documento descargado, y adjúntala a los documentos que entregarás a tu diputado.</p>
-            <p>4. Para registrar otro menor, puedes hacerlo una vez confirmado tu correo electrónico.</p>
-       </div></div></div></div>`;       
+          <h5 class="card-header">NO DEBIAS VER ESTO</h5>
+          </div></div></div>`;       
       // User is signed out.
       // ...
     }
@@ -141,6 +136,7 @@ function singOut(){
   firebase.auth().signOut().then(function() {
     // Sign-out successful.
     console.log(' Saliendo... ')
+location.href = 'user.html'      
   }).catch(function(error) {
     // An error happened.
     var errorCode = error.code;
