82175e80f17b89b0884fcbd9b9bdfb3b32f92389
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-18 22:23:09 -0500

    Update user.js

--- user.js
+++ user.js
@@ -131,7 +131,7 @@ function observer(){
       var uid = user.uid;
       var providerData = user.providerData;
       // ...
-      console.log(`Usuario activo: ${email}, Estado: ${emailVerified}`)
+      console.log(`Usuario activo: ${email}, Estado: ${true}`)
       get_user(email)
         
     } else {
