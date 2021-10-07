f2107169db1d3f2822506f5ef452e2dd9b77adc6
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 16:31:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -94,6 +94,31 @@ function observer(){
     <p class="card-text">Sociedad de Oncología y Hematolología del César</p>
     <p class="card-text">Clínica Porto Azul</p>
   </div>
+  
+  
+  <div class="card-group">
+  <div class="card text-white bg-success mb-3">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  
+  <div class="card text-white bg-success mb-3">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
        
         </div>
        `;       
