5ece5ff2e290390d136de1a94bc26bc41e561ce6
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 16:40:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -85,13 +85,19 @@ function observer(){
           <div class="card-body">
             <p>Perfilamiento de pacientes para inhibidores de Tirosina Quinasa</p>
       </div></div>
-       <div style="padding-bottom:20px;">/div>
-       <div class="card border-success mb-3">
-  <div class="card-header">Header</div>
-  <div class="card-body text-success">
-    <h5 class="card-title">Success card title</h5>
+      
+       <div style="padding-bottom:20px;"></div>
+
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
   </div>
+</div>
        
         </div>
        `;       
