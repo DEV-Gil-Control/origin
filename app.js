c34ded257c81238718ce8f38302bc354618d064e
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 16:21:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -90,8 +90,9 @@ function observer(){
 <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
   <div class="card-header">Header</div>
   <div class="card-body">
-    <h5 class="card-title">Success card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">Dr Ray Manneh</h5>
+    <p class="card-text">Oncólogo Clínico</p>
+    <p class="card-text">Sociedad de Oncología y Hematolología del César</p>
   </div>
        
         </div>
