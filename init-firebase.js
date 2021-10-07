e6441e6158f33c779bb2178ccc148d6d93ee2dfd
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 14:23:33 -0500

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,12 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyBFEGUWXOTv5Sgcae53-MUJmqlPce0YZ0M",
-  authDomain: "amparo-671eb.firebaseapp.com",
-  databaseURL: "https://amparo-671eb.firebaseio.com",
-  projectId: "amparo-671eb",
-  storageBucket: "amparo-671eb.appspot.com",
-  messagingSenderId: "680179086675",
-  appId: "1:680179086675:web:683ba04a352afcd03d1da6"
+apiKey: "AIzaSyC50ia5BSp3uWndhfbQV1QaQNQOqy86RFg",
+    authDomain: "pfizer-oncologia.firebaseapp.com",
+    projectId: "pfizer-oncologia",
+    storageBucket: "pfizer-oncologia.appspot.com",
+    messagingSenderId: "310701170218",
+    appId: "1:310701170218:web:15712e249fedb904326d53"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
