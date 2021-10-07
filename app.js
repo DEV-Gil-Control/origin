ed2ed833d79ba988aff2d0b59e4883926be3de33
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 15:57:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -126,12 +126,10 @@ function observer(){
       content.innerHTML = `
       <div class="container mt-5">
         <div class="card">
-          <h5 class="card-header">Bienvenido al sistema de registro y descarga de documentación, por favor sigue las siguientes instrucciones:</h5>
+          <h5 class="card-header">Bienvenido al conversatorio:</h5>
           <div class="card-body">
-            <p>1. Haz clic en el botón "Registrar" en la parte superior para proceder al registro del/la menor de edad a tu cargo.</p>
-            <p>2. Una vez registrado, tendrás acceso a descargar la documentación legal, solo escribe tu correo, contraseña registrada y haz clic en "Ingresar", una vez descargado el documento en formato PDF, debes imprimirlo y firmarlo.</p>
-            <p>3. Revisa y completa la documentación que se describe al final del documento descargado, y adjúntala a los documentos que entregarás a tu diputado.</p>
-            <p>4. Para registrar otro menor, puedes hacerlo una vez confirmado tu correo electrónico.</p>
+            <h3>Cáncer Renal Metastásico:</h3>
+            <p>Perfilamiento de Pacientes para Inhibidores de Tirosina Quinasa</p>
        </div></div></div></div>`;       
       // User is signed out.
       // ...
