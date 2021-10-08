a7fb54022eb0d64a51f2e71ffd1f0bc9d48d5c35
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-08 03:02:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ function eventos() {
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
-      form.addEventListener('submit', function () {
+      form.addEventListener('submit', function (event)  {
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
