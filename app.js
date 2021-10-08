c0cc52aa01a761150ba42b07fe7e3251a94b959e
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-08 04:01:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -42,3 +42,30 @@ var push_to_firebase = function(data){
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
 
+    $(document).ready(function() {
+  validate();
+  $('input').on('keyup', validate);
+});
+
+function validate() {
+  var inputsWithValues = 0;
+  
+  // get all input fields except for type='submit'
+  var myInputs = $("input:not([type='submit'])");
+
+  myInputs.each(function(e) {
+    // if it has a value, increment the counter
+    if ($(this).val()) {
+      inputsWithValues += 1;
+    }
+  });
+
+  if (inputsWithValues == myInputs.length) {
+    $("input[type=submit]").prop("disabled", false);
+  } else {
+    $("input[type=submit]").prop("disabled", true);
+  }
+}  
+      
+      
+      
