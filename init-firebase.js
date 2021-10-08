77513a7f029f3115abf2654cb0a2a65fe62c57ae
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 20:00:55 -0500

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -2,6 +2,7 @@
 var firebaseConfig = {
 apiKey: "AIzaSyC50ia5BSp3uWndhfbQV1QaQNQOqy86RFg",
     authDomain: "pfizer-oncologia.firebaseapp.com",
+    databaseURL: "https://fizer-oncologia.firebaseapp.com",
     projectId: "pfizer-oncologia",
     storageBucket: "pfizer-oncologia.appspot.com",
     messagingSenderId: "310701170218",
