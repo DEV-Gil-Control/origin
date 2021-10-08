327a72d4a03ea24e30f6ca87c2a8eeb1d8c892f4
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 19:05:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -80,25 +80,22 @@ function observer(){
       console.log('Ningun Usuario Activo')
       content.innerHTML = `
 
-
-
-          <h5 class="modal-title" id="modalRegisterUser">Datos de Acceso</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <input  class="form-control form-control-sm mr-sm-2 mb-1" id="email" type="email" placeholder="Ingresa email" >
-          <input  class="form-control form-control-sm mr-sm-2" id="password" type="password" placeholder="Ingresa Contraseña">
-        </div>
-                  <div class="modal-header">
-                  <h5 class="modal-title" id="modalRegisterUser">Aviso de Privacidad</h5>
-                  </div>
-    <form style="padding-left:5px;">
-        <div>
-    <input type="checkbox" id="privacy" name="subscribe" value="newsletter">
-    <label for="subscribeNews">Acepto Aviso de Privacidad</label>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
    
        `;       
       // User is signed out.
