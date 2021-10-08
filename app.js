1663dc5c3dee222c4b22523c205b4d1b5b95013a
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 23:53:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -44,21 +44,7 @@ var push_to_firebase = function(data){
 
 
 function observer(){
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      show(user);
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-      console.log(`Usuario activo: ${email}, Estado: ${emailVerified}`)
-    } else {
-      console.log('Ningun Usuario Activo')
+ 
       content.innerHTML = `
 <div style="padding-top: 30px;"> </div>
 <div class="card" style="width: 30rem; margin: auto; width: 50%; padding: 10px;">
