4345c46ed04183cdb1926a1067842e5c31a3c028
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-08 01:05:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -43,60 +43,7 @@ var push_to_firebase = function(data){
 
 
 
-function observer(){
- 
-        firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      show(user);
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-      console.log(`Usuario activo: ${email}, Estado: ${emailVerified}`)
-    } else {
-      console.log('Ningun Usuario Activo') 
-        
-      content.innerHTML = `
 
-   
-       `;       
-      // User is signed out.
-      // ...
-    }
-  });
-}
-observer();
-
-
-
-function verification(){
-  var user = firebase.auth().currentUser;
-
-  user.sendEmailVerification().then(function() {
-    // Email sent.
-    console.log(`Enviando correo...`);
-  }).catch(function(error) {
-    // An error happened.
-    console.log(`Error (${error})`);
-
-  });
-}
-
-function show(user) {
-  var user = user;
-  var content = document.getElementById('content');
-
-  if (user.emailVerified) {
-    location.href = 'https://global.gotomeeting.com/join/589781757'
-  }else{
-location.href = 'https://global.gotomeeting.com/join/589781757'
-  }
-}
 
 function eventos() {
  location.href = 'https://global.gotomeeting.com/join/589781757';
