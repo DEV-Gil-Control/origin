9abcc713e553df462860251241c2b6a74fc225a5
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 19:26:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -84,19 +84,19 @@ function observer(){
   <div class="card-body">
 <form>
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="email">Correo Electrónico</label>
+    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Escriba su correo electrónico">
+    <small id="emailHelp" class="form-text text-muted">Sus datos no serán compartidos con nadie.</small>
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="empresa">Empresa para que labora</label>
+    <input type="empresa" class="form-control" id="empresa" placeholder="Empresa">
   </div>
   <div class="form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <label class="form-check-label" for="exampleCheck1">Acepto el Aviso de Privacidad</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Entrar</button>
 </form>
   </div>
 </div>
