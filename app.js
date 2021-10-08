518843a88c1494dc825727a2e6b77304dbec5de4
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-08 08:18:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,16 @@ var push_to_firebase = function(data){
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
-
+function validateForm() {
+var nameVal = document.forms["simpleForm"]["name"].value;
+if(nameVal == null || nameVal == "") {
+document.getElementsByClassName( "errorMessage" )[0].style.visibility = "visible";
+document.getElementsByClassName( "errorMessage" )[0].innerHTML = "Please Fill out this field";
+return false;
+} else {
+return true;
+}
+}
   
       
       
