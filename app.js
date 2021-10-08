4c584eb958af0c5ad9c88a57c8f1535fa305b97d
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-10-07 22:53:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var push_to_firebase = function(data){
-        alert(onClick="parent.location='https://global.gotomeeting.com/join/589781757'");
+        alert("Ya puede entrar a su evento");
 );
         var db = firebase.firestore();
 
